8aa2dcd390e4d37ddeabb8d0c12d00e7f007bf76
Snaiker <56450075+Snaiker@users.noreply.github.com>
2020-06-03 11:35:52 +0200

    Update project

---
+++ GenerateFurnis/Furnis.cs
@@ -0,0 +1,18 @@
+﻿namespace GenerateFurnis
+{
+    public class Furnis
+    {
+        public string className { get; set; }
+
+        public string publicName { get; set; }
+
+        public string descName { get; set; }
+
+        public Furnis(string cN, string pN, string dN)
+        {
+            this.className = cN;
+            this.publicName = pN;
+            this.descName = dN;
+        }
+    }
+}
