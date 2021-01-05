6ada801e0473b5df5584c2f210d04b294a38a9fe
Snaiker <steveraki2000@gmail.com>
2021-01-05 23:19:15 +0000

    Updates

---
+++ GenerateFurnis/bin/Debug/netcoreapp3.1/sqls/items_base.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items_base` VALUES (900375, 900375, 'darkmodern_c20_bed', 'darkmodern_c20_bed', 's', 1, 1, 1.00, 1, 0, 0, 0, 1, 1, 0, 0, 1, 'default', 1, '0', '', '', 0, 0, '');
+INSERT INTO `items_base` VALUES (900376, 900376, 'darkmodern_c20_bunnylight', 'darkmodern_c20_bunnylight', 's', 1, 1, 1.00, 1, 0, 0, 0, 1, 1, 0, 0, 1, 'default', 1, '0', '', '', 0, 0, '');
